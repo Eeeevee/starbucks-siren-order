dab071578cc7441ce44c011c163bb62ea4f908cd
Eevee <156054454+Eeeevee@users.noreply.github.com>
2024-02-07 16:31:29 +0900

    Delete src directory

--- src/Item.java
+++
@@ -1,8 +0,0 @@
-public class Item extends Menu {
-    Double price;
-
-    Item(String menuName, String englishName, Double price) {
-        super(menuName, englishName);
-        this.price = price;
-    }
-}
